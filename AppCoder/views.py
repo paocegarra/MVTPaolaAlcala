4e7968325a125df657bc4462f2259b9065632998
paocegarra <paocegarra2@gmail.com>
2022-10-03 19:26:24 -0300

    sin base de datos

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -12,10 +12,10 @@ from AppCoder.models import Familia
 
 def crear_familia(request):
     
-    familiar_1 = Familia (nombre="Ricardo" , apellido="Medina" , correo="mini@gmail.com", edad="55")
+    familiar_1 = Familia (nombre="Ricardo" , apellido="Medina" , correo="mini@gmail.com", edad="55", fecha_de_nacimiento="1966-7-31")
+    familiar_2 = Familia (nombre="Leandro" , apellido="Medina" , correo="lean@hotmail.com.ar", edad="32", fecha_de_nacimiento="1990-5-15")
+    familiar_3 = Familia (nombre="Mariano" , apellido="Gonsalo" , correo="mari12@yahoo.com", edad="17", fecha_de_nacimiento="2003-10-1")
     
-    familiar_1.save()
-    
-    return HttpResponse(f"Mi papa es: {familiar_1.nombre} {familiar_1.apellido} . Su correo es: {familiar_1.correo} . Su edad es: {familiar_1.edad}  ")
+    return HttpResponse()
     
     
